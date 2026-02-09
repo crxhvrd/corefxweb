66b7dff2bdb1b2cc53883b20c9b8b44e0f9a7dae
crxhvrd <crxhvrd@users.noreply.github.com>
2026-02-09 17:17:47 +0200

    Hide scrollbar on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ImageGrid from '@/components/ImageGrid';
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-screen scrollbar-hide">
       {/* Scrollable Content */}
       <div className="relative pt-screen">
         <ImageGrid />
