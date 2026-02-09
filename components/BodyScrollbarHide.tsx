ea9f5c2a3a69f2c9b85cacda2f20d5d592743f0f
crxhvrd <crxhvrd@users.noreply.github.com>
2026-02-09 17:26:30 +0200

    Target both html and body elements for scrollbar-hide

--- components/BodyScrollbarHide.tsx
+++ components/BodyScrollbarHide.tsx
@@ -9,12 +9,15 @@ export default function BodyScrollbarHide() {
     useEffect(() => {
         // Hide scrollbar on homepage only
         if (pathname === '/') {
+            document.documentElement.classList.add('scrollbar-hide');
             document.body.classList.add('scrollbar-hide');
         } else {
+            document.documentElement.classList.remove('scrollbar-hide');
             document.body.classList.remove('scrollbar-hide');
         }
 
         return () => {
+            document.documentElement.classList.remove('scrollbar-hide');
             document.body.classList.remove('scrollbar-hide');
         };
     }, [pathname]);
